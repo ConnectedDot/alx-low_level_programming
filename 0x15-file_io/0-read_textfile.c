35bb8b35199797d0c4a7a8feafa070cd83ec6642
ConnectedDot <psalmomo@gmail.com>
2023-04-10 22:59:07 -0700

    Add: 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-/**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+    int file_descriptor, bytes_read, bytes_written;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return 0;
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+        return 0;
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+    buffer = malloc(sizeof(char) * (letters + 1));
+    if (buffer == NULL)
+    {
+        close(file_descriptor);
+        return 0;
+    }
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return 0;
+    }
 
-	free(buffer);
-	close(o);
+    buffer[bytes_read] = '\0';
 
-	return (w);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return 0;
+    }
+
+    free(buffer);
+    close(file_descriptor);
+
+    return bytes_read;
 }
-
 
 
