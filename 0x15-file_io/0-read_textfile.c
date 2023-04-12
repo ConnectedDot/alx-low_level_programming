4abdf7acc4cfb2f5c8f94f101ba378c250da215d
ConnectedDote <psalmomo@gmail.comm>
2023-04-12 03:47:41 +0100

    Add: 0_read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
-		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+			return (0);
+	buf = malloc(sizeof(char) * letters)
+	t = read(fd, buf, letters):
+	w = write(STDOUT_FILENO, buf, t);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	free(buffer);
-	close(o);
-
+	free(buf);
+	close(fd);
 	return (w);
+
 }
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,16 @@
+0x15. C - File I/O
+
+##Resources
+###Read or watch:
+
+File descriptors
+C Programming in Linux Tutorial #024 - open() read() write() Functions
+man or help:
+
+open
+close
+read
+write
+dprintf
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google
