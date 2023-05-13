1d412ec8a7ffd2b6d87d826a3b9acacc08e538fa
ConnectedDot <psalmomo@gmail.com>
2023-05-13 05:55:36 +0100

    Added my files1 agai again ooon

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,8 +13,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letter);
-int create_file(const char *filename, char *text_contents);
-int append_text_to_file(const char *filename, char *text_contents);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
