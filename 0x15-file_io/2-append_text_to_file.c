73ab5a6524d95799f7740fba801d1c15e0913151
ConnectedDot <psalmomo@gmail.com>
2023-05-13 05:54:11 +0100

    Added my files1 agai again ooon

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,21 +9,21 @@
  *         If the file does not exist the user lacks write permissions - -1.
  *         Otherwise - 1.
  */
-int append_text_to_file(const char *filename, char *text_contents)
+int append_text_to_file(const char *filename, char *text_content)
 {
     int o, w, len = 0;
 
     if (filename == NULL)
         return (-1);
 
-    if (text_contents != NULL)
+    if (text_content != NULL)
     {
         for (len = 0; text_content[len];)
             len++;
     }
 
     o = open(filename, O_WRONLY | O_APPEND);
-    w = write(o, text_contents, len);
+    w = write(o, text_content, len);
 
     if (o == -1 || w == -1)
         return (-1);
