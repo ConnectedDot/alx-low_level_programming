1c3425bdfacc74d11afa6632033535113663fea0
ConnectedDot <psalmomo@gmail.com>
2023-05-29 12:42:20 +0100

    DDDD

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,24 +11,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int o, w, len = 0;
+	int o, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len];)
-            len++;
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    o = open(filename, O_WRONLY | O_APPEND);
-    w = write(o, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-    if (o == -1 || w == -1)
-        return (-1);
+	if (o == -1 || w == -1)
+		return (-1);
 
-    close(o);
+	close(o);
 
-    return (1);
+	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,13 +11,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters)
-int create_file(const char *filename, char *text_content)
-int append_text_to_file(const char *filename, char *text_content)
-char *create_buffer(char *file)
-
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
